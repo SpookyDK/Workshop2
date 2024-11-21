374546f28f39480d526a8edfade928b2030cdda9
SpookyDK <esben9541@gmail.com>
2024-11-21 20:58:42 +0100

    better

--- fridge.py
+++ fridge.py
@@ -1,23 +1,30 @@
 from electricity import electricity
+import math
+
+
 class fridge:
     def __init__(self, starting_temp: float):
         self.temp: float = [starting_temp]
-        self.C1 = 5*(10**-7)*(300**-1)
-        self.C2 = 3*(10**-5)*(300**-1)
+        self.C1 = 5*(10**-7)
+        self.C2 = 3*(10**-5)
         self.CC1 = 0*(300**-1)
-        self.CC2 = 8*(10**-6)*(300**-1)
+        self.CC2 = 8*(10**-6)
         self.El = electricity()
         self.price = 0
-        print(self.C1)
-        print(self.C2)
-        print(self.CC1)
-        print(self.CC2)
+        self.time_under = 0 
+        self.time_over = 0
 
-    def _should_cool(self, itteration: int) -> bool:
-        return (self.temp[itteration] > 5)
+    def _should_cool(self, itteration: int, closed: bool = True, thermostat: str = "") -> bool:
+        match thermostat:
+            case "Under3":
+                return self.thermostat_under_3(itteration-1)
+            case "Under3Expanded":
+                return self.thermostat_under_3_expanded(itteration-1, closed)
+            case _:
+                return (self.temp[itteration] > 5)
 
-    def _update_temp(self, itteration: int, closed: bool):
-        if self._should_cool(itteration - 1):
+    def _update_temp(self, itteration: int, closed: bool, thermostat: str = ""):
+        if self._should_cool(itteration - 1, closed, thermostat):
             if closed:
                 self.temp.append(self.temp[itteration - 1] + (self.C1 * (20 - self.temp[itteration - 1]) + self.CC2 * (-5 - self.temp[itteration - 1]))*300)
             else:
@@ -28,6 +35,35 @@ class fridge:
                 self.temp.append(self.temp[itteration - 1] + (self.C1 * (20 - self.temp[itteration - 1]) + self.CC1 * (-5 - self.temp[itteration - 1]))*300)
             else:
                 self.temp.append(self.temp[itteration - 1] + (self.C2 * (20 - self.temp[itteration - 1]) + self.CC1 * (-5 - self.temp[itteration - 1]))*300)
+            self._calculate_price(False, itteration)
 
     def _calculate_price(self, running: bool, itteration: int):
-        self.price += self.El.prices[itteration]*1
+        if running:
+            self.price += self.El.prices[itteration]*1  # electricity cost added
+
+        if self.temp[itteration] < 3.5:
+            self.price += 4.39*math.exp(-0.49*self.temp[itteration])
+            self.time_over += 1
+        elif self.temp[itteration] >= 6.5:
+            self.price += 0.11*math.exp(0.31*self.temp[itteration])
+            self.time_over += 1
+
+    def thermostat_under_3(self, itteration: int) -> bool:
+        if self.temp[itteration] >= 6.5:
+            return True
+        elif self.temp[itteration] > 3.5 and self.El.prices[itteration] < 3:
+            return True
+        else:
+            return False
+
+    def thermostat_under_3_expanded(self, itteration: int, closed: bool) -> bool:
+        if self.temp[itteration] >= 6.2:
+            return True
+        elif self.temp[itteration] > 3.5 and self.El.prices[itteration] <2:
+            return True
+        elif self.temp[itteration] > 5 and self.El.prices[itteration] < 3:
+            return True
+        elif self.temp[itteration] > 6.2 and not closed:
+            return True
+        else:
+            return False
--- main.py
+++ main.py
@@ -4,32 +4,50 @@ import matplotlib.pyplot as plt
 
 
 def main():
+    its = 1
+    cost = 0
+    time_under = 0
+    time_over = 0
     print("working")
-    Fridge = fridge(5)
-    print(Fridge.El.prices)
-    for x in range(1, len(Fridge.El.prices)-1):
-        closed = True
-        if random.randrange(0, 9) == 0:
-            closed = False
-        Fridge._update_temp(x, closed)
-    print(Fridge.temp)
+    for y in range(0, its):
+        Fridge = fridge(5)
+        for x in range(1, len(Fridge.El.prices)-1):
+            closed = True
+            if random.randrange(0, 9) == 0:
+                closed = False
+            Fridge._update_temp(x, closed, "Under3Expanded")
+        cost += Fridge.price
+        time_under += Fridge.time_under
+        time_over += Fridge.time_over
+        fig, ax = plt.subplots()
+        ax.plot(range(len(Fridge.temp)), Fridge.temp, color='r')
+        ax.plot(range(len(Fridge.El.prices)), Fridge.El.prices, color='g')
+        ax.set_xlabel("Time (Index)")
+        ax.set_ylabel("Temperature")
+        ax.set_title("Fridge Temperature Over Time")
+        plt.show()
 
 
 
 
 
-    fig, ax = plt.subplots()
-    x_values = range(len(Fridge.temp))  # X-akse værdier fra 0 til længden af Fridge_temp
-    ax.plot(x_values, Fridge.temp)  # Tegn plottet
 
-    # Tilføj akse-labels og titel
-    ax.set_xlabel("Time (Index)")
-    ax.set_ylabel("Temperature")
-    ax.set_title("Fridge Temperature Over Time")
+    print(cost/its)
+    print(time_over/its)
+    print(time_under/its)
+    # fig, ax = plt.subplots()
+    # ax.plot(range(len(Fridge.temp)),  Fridge.temp)
+    # ax.set_xlabel("Time (Index)")
+    # ax.set_ylabel("Temperature")
+    # ax.set_title("Fridge Temperature Over Time")
+    # plt.show()
 
-    # Vis plottet
-    plt.show()
-
+    # fig, ax = plt.subplots()
+    # ax.plot(range(len(Fridge.El.prices)),  Fridge.El.prices)
+    # ax.set_xlabel("Itteration (Index)")
+    # ax.set_ylabel("price")
+    # ax.set_title("Electricty prices over time")
+    # plt.show()
 
 
 
